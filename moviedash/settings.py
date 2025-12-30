7050c4cb386ac9566352a215129c06c985bfe329
Nafis Shahriar <nafisshahriar003@gmail.com>
2025-12-30 08:57:41 +0600

    v1.7

--- moviedash/settings.py
+++ moviedash/settings.py
@@ -6,7 +6,7 @@ print(">>> PROJECT ROOT:", os.path.dirname(os.path.abspath(__file__)))
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dev-secret-key')
-DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'False'
 ALLOWED_HOSTS = ['*'] if DEBUG else os.getenv('DJANGO_ALLOWED_HOSTS','').split(',')
 INSTALLED_APPS = [
     'django.contrib.admin',
